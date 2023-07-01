545fdfddd3086f6d17fc1a3c4152cfc1f1b4ac0d
Nao-RandD <nao.r.and.d@gmail.com>
2023-07-01 14:58:37 +0900

    feat: available to get review by AppInfoList.

--- ChatGPTisGodApp/View/AppInfoView.swift
+++ ChatGPTisGodApp/View/AppInfoView.swift
@@ -11,10 +11,10 @@ import AppInfoList
 struct AppInfoView: View {
     let info = AppInfo(
         termOfUseURL: URL(string: "https://nao-randd.github.io/ChatGPTisGodApp/"),
-//        appURL: URL(string: "https://apps.apple.com/jp/app/ez-pomo/id1668609447?l=ja")!,
-           developerInfoURL: URL(string: "https://twitter.com/Nao_RandD")!
-//           appStoreID: "1668609447"
-        )
+        appURL: URL(string: "https://apps.apple.com/jp/app/ai%E3%81%8A%E6%82%A9%E3%81%BF%E7%9B%B8%E8%AB%87/id6446774783")!,
+        developerInfoURL: URL(string: "https://twitter.com/Nao_RandD"),
+        appStoreID: "6446774783"
+    )
     let fileURL = Bundle.main.url(forResource: "license-list", withExtension: "plist")!
 
     var body: some View {
