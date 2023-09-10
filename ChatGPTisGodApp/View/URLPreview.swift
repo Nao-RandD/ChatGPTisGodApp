aad7d977685324119f4258743a852a4d00e9e617
Nao-RandD <nao.r.and.d@gmail.com>
2023-09-10 16:15:45 +0900

    add: Add Resources to App Clip

---
+++ ChatGPTisGodApp/View/URLPreview.swift
@@ -0,0 +1,18 @@
+//
+//  URLPreview.swift
+//  ChatGPTisGodApp
+//
+//  Created by Nao RandD on 2023/09/10.
+//
+
+import SwiftUI
+
+struct URLPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    URLPreview()
+}
